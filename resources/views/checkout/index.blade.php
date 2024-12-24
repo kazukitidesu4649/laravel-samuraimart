6513558d400ce673decff05099a2c8b78260f4a7
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-12-24 10:06:18 +0900

    配布された添付ファイルを上書き保存。 public & resourcesフォルダ

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -23,23 +23,23 @@
                                     <div class="col-md-3">
                                         <a href="{{ route('products.show', $product->id) }}">
                                             @if ($product->options->image)
-                                                <img src="{{ asset($product->options->image) }}" class="img-thumbnail">
+                                                <img src="{{ asset($product->options->image) }}" class="img-thumbnail samuraimart-product-img-cart">
                                             @else
-                                                <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
+                                                <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail samuraimart-product-img-cart">
                                             @endif
                                         </a>
                                     </div>
                                     <div class="col-md-9">
                                         <div class="flex-column">
                                             <p class="fs-5 mb-2">
-                                                <a href="{{ route('products.show', $product->id) }}" class="link-dark text-decoration-none">{{ $product->name }}</a>
+                                                <a href="{{ route('products.show', $product->id) }}" class="link-dark">{{ $product->name }}</a>
                                             </p>
                                             <div class="row mb-2">
-                                                <div class="col-xxl-3">
-                                                    数量:{{ number_format($product->qty) }}
+                                                <div class="col-xxl-2">
+                                                    数量：{{ number_format($product->qty) }}
                                                 </div>
-                                                <div class="col-xxl-9">
-                                                    合計:￥{{ number_format($product->qty * $product->price) }}
+                                                <div class="col-xxl-10">
+                                                    合計：￥{{ number_format($product->qty * $product->price) }}
                                                 </div>
                                             </div>
                                         </div>
@@ -54,39 +54,39 @@
                     <hr class="mb-4">
 
                     <div class="mb-5">
-                        <p class="mb-2">{{ Auth::user()->name }}様</p>
-                        <p class="mb-2">〒{{ Auth::user()->postal_code  . ' ' . Auth::user()->address }}</p>
-                        <p class="mb-2">{{ Auth::user()->phone}}</p>
-                        <p>{{ Auth::user()->email}}</p>
+                        <p class="mb-2">{{ Auth::user()->name }} 様</p>
+                        <p class="mb-2">〒{{ Auth::user()->postal_code . ' ' . Auth::user()->address }}</p>
+                        <p class="mb-2">{{ Auth::user()->phone }}</p>
+                        <p>{{ Auth::user()->email }}</p>
                     </div>
                 </div>
                 <div class="col col-xxl-4">
-                    <div class="bg-white p-4 mb-4">
+                    <div class="bg-light p-4 mb-4">
                         <div class="row mb-2">
-                              <div class="col-md-5">
+                            <div class="col-md-5">
                                 小計
-                              </div>
-                              <div class="col-md-7">
-                                ¥{{ number_format($total - $carriage_cost) }}
-                              </div>
+                            </div>
+                            <div class="col-md-7">
+                                ￥{{ number_format($total - $carriage_cost) }}
+                            </div>
                         </div>
 
                         <div class="row mb-3">
-                              <div class="col-md-5">
+                            <div class="col-md-5">
                                 送料
-                              </div>
-                              <div class="col-md-7">
-                                ¥{{ number_format($carriage_cost) }}
-                              </div>
+                            </div>
+                            <div class="col-md-7">
+                                ￥{{ number_format($carriage_cost) }}
+                            </div>
                         </div>
 
                         <div class="row">
-                              <div class="col-md-5">
-                                  <span class="fs-5 fw-bold">合計</span>
-                              </div>
-                              <div class="col-7 d-flex align-items-center">
-                                  <span class="fs-5 fw-bold">¥{{ number_format($total) }}</span><span class="small">(税込)</span>
-                              </div>
+                            <div class="col-5">
+                                <span class="fs-5 fw-bold">合計</span>
+                            </div>
+                            <div class="col-7 d-flex align-items-center">
+                                <span class="fs-5 fw-bold">￥{{ number_format($total) }}</span><span class="small">（税込）</span>
+                            </div>
                         </div>
                     </div>
 
@@ -97,7 +97,7 @@
                                 <button type="submit" class="btn samuraimart-submit-button text-white w-100">お支払い</a>
                             </form>
                         @else
-                            <button class="btn samuraimart-submit-button disabled w-100">お支払い</button>
+                            <button class="btn samuraimart-submit-button-disabled w-100">お支払い</button>
                         @endif
                     </div>
                 </div>
